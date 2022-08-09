da981b76f2e7d5f493e785b1f5ddff911e7d7580
kashyappathak <77912420+kashyappathak@users.noreply.github.com>
2022-08-09 22:14:15 +0530

    Add Files via upload

---
+++ operators/arithmetic.dart
@@ -0,0 +1,16 @@
+void main() { 
+   var num1 = 10; 
+   var num2 = 2; 
+   var res = 0; 
+   
+   res = num1+num2; 
+   print("Addition: ${res}"); 
+   
+   res = num1-num2;
+   print("Subtraction: ${res}"); 
+   
+   res = num1*num2; 
+   print("Multiplication: ${res}"); 
+   
+   res = num1/num2; 
+   print("Division: ${res}"); 
