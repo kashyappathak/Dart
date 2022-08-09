39d832a278ea6953229002c8a396e5cfffd7c53a
kashyappathak <77912420+kashyappathak@users.noreply.github.com>
2022-08-09 22:26:26 +0530

    Add files via upload

---
+++ loops/loop3.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+
+void main(){
+    int i=0;
+    for(i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            stdout.write(j+1);
+        }
+        print("\n");
+    }
+}
