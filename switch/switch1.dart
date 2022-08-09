6e0a68f9d9080c7f616abd48adbec56d0deb0c07
kashyappathak <77912420+kashyappathak@users.noreply.github.com>
2022-08-09 22:30:37 +0530

    Add files via upload

---
+++ switch/switch1.dart
@@ -0,0 +1,44 @@
+void main()
+{
+  int number=2;
+
+  switch(number)
+  {
+    case 1:
+    print("31 Days.");
+    break;
+    case 2:
+    print("28 Days For non-leap year, for leap 29 days");
+    break;
+    case 3:
+    print("31 Days.");
+    break;
+    case 4:
+    print("30 Days.");
+    break;
+    case 5:
+    print("31 Days.");
+    break;
+    case 6:
+    print("30 Days.");
+    break;
+    case 7:
+    print("31 Days.");
+    break;
+    case 8:
+    print("31 Days.");
+    break;
+    case 9:
+    print("30 Days.");
+    break;
+    case 10:
+    print("31 Days.");
+    break;
+    case 11:
+    print("30 Days.");
+    break;
+    case 12:
+    print("31 Days.");
+    break;
+  }
+}
