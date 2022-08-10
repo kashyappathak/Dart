5e6cef39083911fb3df104373515d570ba5517ad
kashyappathak <77912420+kashyappathak@users.noreply.github.com>
2022-08-10 11:16:13 +0530

    Add files via upload

---
+++ UserValue/properties/properties.dart
@@ -0,0 +1,9 @@
+void main(){
+    var mylist = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10];
+    print(mylist.first);
+    print(mylist.last);
+    print(mylist.isEmpty);
+    print(mylist.isNotEmpty);
+    print(mylist.length);
+    print(mylist.reversed);
+}
