db3656c4d876a41f3b9bb24a732b83c293a84248
kashyappathak <77912420+kashyappathak@users.noreply.github.com>
2022-08-10 10:41:31 +0530

    Add Files via upload

---
+++ UserValue/user1.dart
@@ -0,0 +1,7 @@
+import 'dart:io'; // you can import dart inputout library
+
+void main(){
+    print("Enter Your Name:");
+    var name =  stdin.readLineSync(); // we can import standard input take input form user
+    print("Hello , $name");
+}
